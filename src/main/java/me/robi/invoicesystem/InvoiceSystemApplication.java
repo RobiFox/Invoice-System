5e92ec819af3b8670a810a77ed2ad4a8d2ebd3fe
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-15 17:43:42 +0100

    hardcoded repository, listing get mapping

--- src/main/java/me/robi/invoicesystem/InvoiceSystemApplication.java
+++ src/main/java/me/robi/invoicesystem/InvoiceSystemApplication.java
@@ -1,11 +1,23 @@
 package me.robi.invoicesystem;
 
+import me.robi.invoicesystem.entities.ProductEntity;
+import me.robi.invoicesystem.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InvoiceSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InvoiceSystemApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner fillProductRepo(ProductRepository repository) {
+		return (args -> {
+			repository.save(new ProductEntity("Item 1", 11));
+			repository.save(new ProductEntity("Item 2", 18));
+		});
 	}
 }
--- src/main/java/me/robi/invoicesystem/controllers/InvoiceController.java
+++ src/main/java/me/robi/invoicesystem/controllers/InvoiceController.java
@@ -1,9 +1,22 @@
 package me.robi.invoicesystem.controllers;
 
+import me.robi.invoicesystem.entities.ProductEntity;
+import me.robi.invoicesystem.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class InvoiceController {
+    @Autowired
+    public ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public Iterable<ProductEntity> getProductRepository() {
+        return productRepository.findAll();
+    }
 }

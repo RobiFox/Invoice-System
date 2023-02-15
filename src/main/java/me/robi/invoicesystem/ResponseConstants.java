8ad8d08ae90a49a3e3d4705fff84459f08c6fb14
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-15 18:25:06 +0100

    initial work on invoice generator

---
+++ src/main/java/me/robi/invoicesystem/ResponseConstants.java
@@ -0,0 +1,10 @@
+package me.robi.invoicesystem;
+
+public class ResponseConstants {
+    public static final String RESPONSE_STATUS = "status";
+
+    public static class InvoiceResponseConstants {
+        public static final String PRODUCTS_LIST = "productsList";
+        public static final String PRODUCTS_SUM = "amountSum";
+    }
+}
--- src/main/java/me/robi/invoicesystem/controllers/InvoiceController.java
+++ src/main/java/me/robi/invoicesystem/controllers/InvoiceController.java
@@ -1,11 +1,14 @@
 package me.robi.invoicesystem.controllers;
 
+import me.robi.invoicesystem.ResponseConstants;
 import me.robi.invoicesystem.entities.ProductEntity;
 import me.robi.invoicesystem.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +19,25 @@ public class InvoiceController {
     @GetMapping("/products")
     public Iterable<ProductEntity> getProductRepository() {
         return productRepository.findAll();
+    }
+
+    @GetMapping("/invoice")
+    public ResponseEntity createInvoice(@RequestParam long[] id) {
+        List<ProductEntity> entities = new ArrayList<>();
+        int amountSum = 0;
+
+        for(long l : id) {
+            ProductEntity product = productRepository.findById(l).orElse(null);
+            if(product == null)
+                return new ResponseEntity(Collections.singletonMap(ResponseConstants.RESPONSE_STATUS, String.format("Product of ID %s not found.", l)), HttpStatus.BAD_REQUEST);
+            entities.add(product);
+            amountSum += product.getAmount();
+        }
+
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put(ResponseConstants.InvoiceResponseConstants.PRODUCTS_SUM, amountSum);
+        responseBody.put(ResponseConstants.InvoiceResponseConstants.PRODUCTS_LIST, entities);
+
+        return new ResponseEntity(responseBody, HttpStatus.OK);
     }
 }

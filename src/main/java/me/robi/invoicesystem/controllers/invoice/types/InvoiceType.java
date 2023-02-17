69a2182fafa73e7bca13f08c2d75891d4aba41ac
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-17 20:49:19 +0100

    updated abstract class so i can store a singular object for each response method

--- src/main/java/me/robi/invoicesystem/controllers/invoice/types/InvoiceType.java
+++ src/main/java/me/robi/invoicesystem/controllers/invoice/types/InvoiceType.java
@@ -1,5 +1,6 @@
 package me.robi.invoicesystem.controllers.invoice.types;
 
+import jakarta.servlet.http.HttpServletRequest;
 import me.robi.invoicesystem.entities.ProductEntity;
 import me.robi.invoicesystem.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,32 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class InvoiceType {
-    private List<ProductEntity> entities;
-    private int totalSum;
-
     @Autowired
     private ProductRepository productRepository;
 
-    public abstract ResponseEntity getResponse();
-
-    public InvoiceType(long[] id) {
-        entities = new ArrayList<>();
-        totalSum = 0;
-
-        for(long l : id) {
-            ProductEntity product = productRepository.findById(l).orElse(null);
-            if(product == null)
-                throw new NullPointerException(String.format("Product of ID %s not found.", l));
-            entities.add(product);
-            totalSum += product.getAmount();
-        }
-    }
-
-    public int getTotalSum() {
-        return totalSum;
-    }
-
-    public List<ProductEntity> getEntities() {
-        return entities;
-    }
+    public abstract ResponseEntity getResponse(HttpServletRequest request, List<ProductEntity> entities, int totalSum);
 }
--- src/main/java/me/robi/invoicesystem/controllers/invoice/types/RawInvoiceType.java
+++ src/main/java/me/robi/invoicesystem/controllers/invoice/types/RawInvoiceType.java
@@ -1,24 +1,23 @@
 package me.robi.invoicesystem.controllers.invoice.types;
 
+import jakarta.servlet.http.HttpServletRequest;
+import me.robi.invoicesystem.entities.ProductEntity;
 import org.springframework.http.ResponseEntity;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static me.robi.invoicesystem.constants.ResponseConstants.InvoiceResponseConstants.PRODUCTS_LIST;
 import static me.robi.invoicesystem.constants.ResponseConstants.InvoiceResponseConstants.PRODUCTS_SUM;
 
 public class RawInvoiceType extends InvoiceType {
-    public RawInvoiceType(long[] id) {
-        super(id);
-    }
-
     @Override
-    public ResponseEntity getResponse() {
+    public ResponseEntity getResponse(HttpServletRequest request, List<ProductEntity> entities, int totalSum) {
         Map<String, Object> responseBody = new HashMap<>();
 
-        responseBody.put(PRODUCTS_SUM, getTotalSum());
-        responseBody.put(PRODUCTS_LIST, getEntities());
+        responseBody.put(PRODUCTS_LIST, entities);
+        responseBody.put(PRODUCTS_SUM, totalSum);
 
         return ResponseEntity.ok(responseBody);
     }

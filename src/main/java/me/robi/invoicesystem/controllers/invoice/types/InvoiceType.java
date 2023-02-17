28410b7acc7ba95cf3d888376287ee958fef2361
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-17 20:57:41 +0100

    cleaned imports

--- src/main/java/me/robi/invoicesystem/controllers/invoice/types/InvoiceType.java
+++ src/main/java/me/robi/invoicesystem/controllers/invoice/types/InvoiceType.java
@@ -2,11 +2,8 @@ package me.robi.invoicesystem.controllers.invoice.types;
 
 import jakarta.servlet.http.HttpServletRequest;
 import me.robi.invoicesystem.entities.ProductEntity;
-import me.robi.invoicesystem.repositories.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class InvoiceType {

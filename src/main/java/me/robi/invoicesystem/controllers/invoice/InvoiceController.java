55bd916087c8fb708c6f606caa240a733333c0a1
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-17 21:04:52 +0100

    fix merge conflict

--- src/main/java/me/robi/invoicesystem/controllers/invoice/InvoiceController.java
+++ src/main/java/me/robi/invoicesystem/controllers/invoice/InvoiceController.java
@@ -30,6 +30,10 @@ import java.util.List;
 import static me.robi.invoicesystem.constants.ResponseConstants.InvoiceResponseConstants.*;
 import static me.robi.invoicesystem.constants.ResponseConstants.*;
 
+/**
+ * This class handles everything related to invoice
+ * and their generation.
+ */
 @RestController
 @RequestMapping("/api")
 public class InvoiceController {
@@ -42,11 +46,23 @@ public class InvoiceController {
         INVOICE_TYPES.put(InvoiceType.PDF_INVOICE, new PdfInvoiceType());
     }
 
+    /**
+     * Lists all products that are available in the repository
+     * @return List of all products with all their fields.
+     */
     @GetMapping("/products")
     public Iterable<ProductEntity> getProductRepository() {
         return productRepository.findAll();
     }
 
+    /**
+     * Base method for filtering products based on an array of ID.
+     * Alternatively also returns them as a JSON if accessed via a GET request.
+     * @param request The HttpServletRequest provided by Spring
+     * @param type Type of Response Type, from the list of {@link #INVOICE_TYPES}
+     * @param id An array of longs, containing the specified IDs of products.
+     * @return List of filtered products based on {@param id} and a response type of {@param type}.
+     */
     @GetMapping({"/invoice", "/invoice/{type}"})
     public ResponseEntity<Map<String, Object>> createInvoiceBase(HttpServletRequest request, @PathVariable(required = false, value = "type") String type, @RequestParam long[] id) {
         if(type == null)
@@ -71,6 +87,12 @@ public class InvoiceController {
         return invoiceType.getResponse(request, entities, amountSum);
     }
 
+    /**
+     * Accesses the given file found in {@link PathConstants#PDF_FILE_STORAGE}, making
+     * sure it's a valid file with a .pdf extension.
+     * @param fileName Name of the file, with an optional .pdf extension at the end
+     * @return An error message if the file is missing, or on invalid file format (illegal characters), or the contents of the pdf file found in {@link PathConstants#PDF_FILE_STORAGE}/{@param fileName}
+     */
     @GetMapping("/access-pdf/{file}")
     public ResponseEntity accessPdf(@PathVariable(value = "file") String fileName) {
         if(!fileName.endsWith(".pdf"))
@@ -90,6 +112,12 @@ public class InvoiceController {
         }
     }
 
+    /**
+     * Verifies the validity of a file name from an user input.
+     * Calls {@link #verifyFileName(String, String)} with an automatically parsed file extension as the second parameter, if it exists.
+     * @param fileName The file name from user input
+     * @return {@code true} if the file doesn't contain illegal characters, {@code false} otherwise.
+     */
     public boolean verifyFileName(String fileName) {
         String extension = "";
         if(fileName.contains(".")) {
@@ -99,6 +127,13 @@ public class InvoiceController {
         return verifyFileName(fileName, extension);
     }
 
+    /**
+     * Verifies the validity of a file name from an user input.
+     * File name may only contain letters, hyphen or underscore, while the extension may only contain letters.
+     * @param fileName The file name from user input
+     * @param fileExtensionRegex The extension of the file
+     * @return {@code true} if the file doesn't contain illegal characters, {@code false} otherwise.
+     */
     public boolean verifyFileName(String fileName, String fileExtensionRegex) {
         return fileName.matches("[\\w\\-_]+" + (fileExtensionRegex.length() > 0 ? "\\.\\w+" : ""));
     }

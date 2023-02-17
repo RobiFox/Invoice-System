ace1d0aae3c5f8b00249dff4ac5d173fe074d151
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-17 21:29:34 +0100

    moved access-pdf mappings to PdfInvoiceType

--- src/main/java/me/robi/invoicesystem/controllers/invoice/types/PdfInvoiceType.java
+++ src/main/java/me/robi/invoicesystem/controllers/invoice/types/PdfInvoiceType.java
@@ -8,7 +8,12 @@ import com.itextpdf.text.pdf.draw.LineSeparator;
 import jakarta.servlet.http.HttpServletRequest;
 import me.robi.invoicesystem.constants.PathConstants;
 import me.robi.invoicesystem.entities.ProductEntity;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.File;
@@ -28,7 +33,12 @@ import static me.robi.invoicesystem.constants.ResponseConstants.RESPONSE_STATUS;
  * An Invoice Type that generates a PDF file
  * and saves it at {@link PathConstants#PDF_FILE_STORAGE},
  * returning a URL path to it.
+ * 
+ * Contains Mappings for access-pdf mapping since that mapping is specific
+ * to the PDF invoice type.
  */
+@RestController
+@RequestMapping("/api")
 public class PdfInvoiceType extends InvoiceType {
     /**
      * Returns link to the PDF file. If an exact file
@@ -113,5 +123,56 @@ public class PdfInvoiceType extends InvoiceType {
             cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
             table.addCell(cell);
         }
+    }
+
+    /**
+     * Accesses the given file found in {@link PathConstants#PDF_FILE_STORAGE}, making
+     * sure it's a valid file with a .pdf extension.
+     * @param fileName Name of the file, with an optional .pdf extension at the end
+     * @return An error message if the file is missing, or on invalid file format (illegal characters), or the contents of the pdf file found in {@link PathConstants#PDF_FILE_STORAGE}/{@param fileName}
+     */
+    @GetMapping("/access-pdf/{file}")
+    public ResponseEntity accessPdf(@PathVariable(value = "file") String fileName) {
+        if(!fileName.endsWith(".pdf"))
+            fileName = fileName + ".pdf";
+        if(!verifyFileName(fileName))
+            return ResponseEntity.badRequest().body(Collections.singletonMap(RESPONSE_STATUS, "Illegal file access"));
+
+        Path path = Paths.get(PathConstants.PDF_FILE_STORAGE, fileName);
+
+        if(!Files.exists(path))
+            return ResponseEntity.badRequest().body(Collections.singletonMap(RESPONSE_STATUS, String.format("File %s does not exist.", fileName)));
+
+        try {
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).body(Files.readAllBytes(path));
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().body(Collections.singletonMap(RESPONSE_STATUS, String.format("Runtime Exception (%s): %s", e.getClass().getName(), e.getMessage())));
+        }
+    }
+
+    /**
+     * Verifies the validity of a file name from an user input.
+     * Calls {@link #verifyFileName(String, String)} with an automatically parsed file extension as the second parameter, if it exists.
+     * @param fileName The file name from user input
+     * @return {@code true} if the file doesn't contain illegal characters, {@code false} otherwise.
+     */
+    public boolean verifyFileName(String fileName) {
+        String extension = "";
+        if(fileName.contains(".")) {
+            String[] split = fileName.split("\\.");
+            extension = split[split.length - 1];
+        }
+        return verifyFileName(fileName, extension);
+    }
+
+    /**
+     * Verifies the validity of a file name from an user input.
+     * File name may only contain letters, hyphen or underscore, while the extension may only contain letters.
+     * @param fileName The file name from user input
+     * @param fileExtensionRegex The extension of the file
+     * @return {@code true} if the file doesn't contain illegal characters, {@code false} otherwise.
+     */
+    public boolean verifyFileName(String fileName, String fileExtensionRegex) {
+        return fileName.matches("[\\w\\-_]+" + (fileExtensionRegex.length() > 0 ? "\\.\\w+" : ""));
     }
 }
